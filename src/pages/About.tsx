e6377b6aa422f5b8f782339edfc4701dec424a4a
DivyanshM221 <142775505+DivyanshM221@users.noreply.github.com>
2025-01-28 01:40:50 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -64,7 +64,7 @@ export default function About() {
             <div className="grid md:grid-cols-2 gap-12 items-center">
               <div className="relative">
                 <img
-                  src="https://gcdnb.pbrd.co/images/MM8115p5Mhxn.jpg?o=1"
+                  src="https://gcdnb.pbrd.co/images/FiYbeGMytiKE.png?o=1"
                   alt="Mr. Shailesh Mehta"
                   className="rounded-2xl shadow-2xl"
                 />
